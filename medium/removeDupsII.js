4d2a6934ad884ee090fb4333b7c92150af29a9b2
William Valadez <billy.valadez@icloud.com>
2023-06-06 23:02:07 -0400

    added comments for future reference

--- medium/removeDupsII.js
+++ medium/removeDupsII.js
@@ -4,8 +4,10 @@ const removeDuplicates = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i + 1]) {
       count++;
+      // if count is greater than two delete num
       if (count > 1) {
         nums.splice(i, 1);
+        // Decrement the index to reevaluate the current position after removing the duplicate.
         i--;
       }
     } else {
