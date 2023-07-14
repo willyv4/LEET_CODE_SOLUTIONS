9c5e9f58c9ead0c62e41dc8566864eda7149ce72
William Valadez <billy.valadez@icloud.com>
2023-07-13 22:14:43 -0400

    better majority element solution

--- easy/LengthOfLastWord.js
+++ easy/LengthOfLastWord.js
@@ -4,3 +4,8 @@ const lengthOfLastWord = (s) => {
   const eachWord = s.split(" ").filter((w) => w !== "");
   return eachWord[eachWord.length - 1].split("").length;
 };
+
+const lengthOfLastWord2 = (s) => s.trim().split(" ").pop().length;
+
+const nospaces = case1.trim().split(" ").pop().length;
+console.log(nospaces);
--- easy/MajorityElement.js
+++ easy/MajorityElement.js
@@ -9,3 +9,19 @@ const majorityElement = function (nums) {
     if (obj[key] > nums.length / 2) return key;
   }
 };
+
+// solution i like better
+const majorityElement = function (nums) {
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!obj[nums[i]]) obj[nums[i]] = 0;
+
+    obj[nums[i]] += 1;
+
+    if (obj[nums[i]] > nums.length / 2) return nums[i];
+  }
+};
+
+// majorityElement([3, 2, 3]); // 3
+majorityElement([2, 2, 1, 1, 1, 2, 2]); // 2
