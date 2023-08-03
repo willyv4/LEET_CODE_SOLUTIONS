715024fe991221cfa0fe103197f42754806f4258
William Valadez <billy.valadez@icloud.com>
2023-08-03 17:56:27 -0400

    found optimal solutions

--- easy/removeDupsFromSortedArr.js
+++ easy/removeDupsFromSortedArr.js
@@ -8,10 +8,72 @@
  */
 
 const removeDuplicates = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    while (nums[i] === nums[i + 1]) {
-      if (i !== -1) nums.splice(i, 1);
-    }
-  }
-  return nums.length;
+	for (let i = 0; i < nums.length; i++) {
+		while (nums[i] === nums[i + 1]) {
+			if (i !== -1) nums.splice(i, 1);
+		}
+	}
+	return nums.length;
 };
+
+/**
+ *
+ * input nums arr
+ * output returns lenght of arr
+ *
+ * example 1 [1,5,5,6,7,7] => [1,5,6,7] (4)
+ *
+ * example 2 [1,1,2] => [1,2] (2)
+ *
+ * exmaple 3
+ *
+ */
+
+// loop over array
+// let currnum
+// let nextnum = currnum + 1
+
+// if nextnum = currnum
+// delete currnum
+// else
+// currnum++
+
+// return nums.length
+
+/**
+ * I1:  currnum = 1
+ * 		nextnum = 5
+ *
+ * I2: currnum = 5
+ * 	   nextnum = 5
+ *
+ * 	   [1,_,5,6,7,7]
+ *
+ *
+ *
+ */
+
+// function removeDups(arr) {
+// 	let j = 0;
+
+// 	while (i < arr.length - 1) arr[i] === arr[i + 1] ? arr.splice(i, 1) : i++;
+
+// 	return arr.length;
+// }
+
+const removeDups = function (nums) {
+	let j = 1; // Initialize a variable 'j' to keep track of the next position for non-duplicate elements.
+
+	for (let i = 0; i < nums.length; i++) {
+		// Loop through the input array 'nums'.
+		if (nums[j - 1] !== nums[i]) {
+			// Check if the current element 'nums[i]' is different from the previous non-duplicate element.
+			nums[j] = nums[i]; // If yes, assign 'nums[i]' to the next position in the array, indicated by 'j'.
+			j++; // Increment 'j' to move to the next position for the next non-duplicate element.
+		}
+	}
+
+	return j; // 'j' represents the length of the modified array, containing only unique elements.
+};
+
+removeDups([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
--- easy/removeElement_27.js
+++ easy/removeElement_27.js
@@ -5,23 +5,36 @@
  */
 
 const removeElement = (nums, val) => {
-  while (nums.includes(val)) {
-    const index = nums.indexOf(val);
-    if (index !== -1) nums.splice(index, 1);
-  }
+	while (nums.includes(val)) {
+		const index = nums.indexOf(val);
+		if (index !== -1) nums.splice(index, 1);
+	}
 
-  return nums.length;
+	return nums.length;
 };
 
 const removeElement2 = (nums, val) => {
-  let i = 0;
+	let i = 0;
 
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] !== val) {
-      nums[i] = nums[j];
-      i++;
-    }
-  }
+	for (let j = 0; j < nums.length; j++) {
+		if (nums[j] !== val) {
+			nums[i] = nums[j];
+			i++;
+		}
+	}
 
-  return i;
+	return i;
 };
+
+function removeElement3(nums, val) {
+	let count = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== val) {
+			nums[count] = nums[i];
+			count++;
+		}
+	}
+
+	return count;
+}
