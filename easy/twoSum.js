2f548129a44aff2438dfd0723792184d27319ba8
William Valadez <billy.valadez@icloud.com>
2023-08-08 21:17:09 -0400

    twosum

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -3,13 +3,32 @@
  * @param {number} target
  * @return {number[]}
  */
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       number = nums[i] + nums[j];
+//       if (number === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
 function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      number = nums[i] + nums[j];
-      if (number === target) {
-        return [i, j];
-      }
-    }
-  }
+	const complementMap = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		const num = nums[i];
+		const complement = target - num;
+
+		if (complementMap.hasOwnProperty(complement)) {
+			return [complementMap[complement], i];
+		}
+
+		complementMap[num] = i;
+	}
+
+	return []; // Return an empty array if no solution is found
 }
+
+console.log(twoSum([1, 2, 3, 4, 5, 6], 3));
