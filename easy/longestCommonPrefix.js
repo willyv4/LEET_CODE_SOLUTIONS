c453fa50107664bec54c51a0ca3724dd01dcea8b
William Valadez <billy.valadez@icloud.com>
2023-11-01 19:16:11 -0400

    .

--- easy/longestCommonPrefix.js
+++ easy/longestCommonPrefix.js
@@ -4,30 +4,48 @@
  * If there is no common prefix, return an empty string "".
  */
 
-const longestCommonPrefix = function (strs) {
-	if (strs.length === 0) return "";
+// const longestCommonPrefix = function (strs) {
+// 	if (strs.length === 0) return "";
 
-	let ans = strs[0];
-	for (let i = 1; i < strs.length; i++) {
-		while (strs[i].indexOf(ans) !== 0) {
-			ans = ans.substring(0, ans.length - 1);
-			if (ans === "") return "";
-		}
-	}
-	return ans;
+// 	let ans = strs[0];
+// 	for (let i = 1; i < strs.length; i++) {
+// 		while (strs[i].indexOf(ans) !== 0) {
+// 			ans = ans.substring(0, ans.length - 1);
+// 			if (ans === "") return "";
+// 		}
+// 	}
+// 	return ans;
+// };
+// const longestCommonPrefix = function (strs) {
+// 	if (strs.length === 0) {
+// 		return "";
+// 	}
+// 	let ans = strs[0];
+// 	for (let i = 1; i < strs.length; i++) {
+// 		while (strs[i].indexOf(ans) !== 0) {
+// 			ans = ans.substring(0, ans.length - 1);
+// 			if (ans === "") {
+// 				return "";
+// 			}
+// 		}
+// 	}
+// 	return ans;
+// };
+
+const longestCommonPrefix = (arr) => {
+  if (arr.length === 0) return "";
+
+  for (let j = 0; j < arr[0].length; j++) {
+    for (let i = 1; i < arr.length; i++) {
+      if (j >= arr[i].length || arr[0][j] !== arr[i][j]) {
+        return arr[0].substring(0, j);
+      }
+    }
+  }
+  return arr[0];
 };
-const longestCommonPrefix = function (strs) {
-	if (strs.length === 0) {
-		return "";
-	}
-	let ans = strs[0];
-	for (let i = 1; i < strs.length; i++) {
-		while (strs[i].indexOf(ans) !== 0) {
-			ans = ans.substring(0, ans.length - 1);
-			if (ans === "") {
-				return "";
-			}
-		}
-	}
-	return ans;
-};
+
+// Call the function and log the result
+const inputArray = ["flower", "flow", "flight"];
+const result = longestCommonPrefix(inputArray);
+console.log(result);
