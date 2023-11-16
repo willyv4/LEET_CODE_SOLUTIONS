7a19371e92e844c3007f2dff3597f9332eafb6d2
William Valadez <billy.valadez@icloud.com>
2023-11-15 20:04:30 -0500

    .

--- medium/threeSum.js
+++ medium/threeSum.js
@@ -4,24 +4,33 @@
  */
 const threeSum = function (nums) {
   let result = [];
+  // sort nums
   nums.sort((a, b) => a - b);
 
+  // loop through nums
   for (let i = 0; i < nums.length; i++) {
+    // if index greater than 0 and num and next === continue
     if (i > 0 && nums[i] === nums[i - 1]) continue;
 
     let left = i + 1;
     let right = nums.length - 1;
 
     while (left < right) {
+      // sum current num and left and right
       const threeSum = nums[i] + nums[left] + nums[right];
 
+      // if threeSum greater than 0 right dectrement
       if (threeSum > 0) {
         right--;
+        // if threeSum less than 0 left increment
       } else if (threeSum < 0) {
         left++;
       } else {
+        // else push [curr, left, right]
         result.push([nums[i], nums[left], nums[right]]);
+        // increment left
         left++;
+        // while left === left next && left less than right left++
         while (nums[left] === nums[left - 1] && left < right) left++;
       }
     }
